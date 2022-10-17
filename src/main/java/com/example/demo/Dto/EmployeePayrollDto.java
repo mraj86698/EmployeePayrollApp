53d4f3852a8a2c3283474dbe3647f993d005ed61
MohanRaj <mraj86698@gmail.com>
2022-10-17 10:52:54 +0530

    Added to Test from Rest Calls for Client Url

---
+++ src/main/java/com/example/demo/Dto/EmployeePayrollDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.Dto;
+
+public class EmployeePayrollDto {
+	public String empName;
+	public long salary;
+
+	public EmployeePayrollDto(String name, long salary) {
+		this.empName = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollData{" + "name='" + empName + '\'' + ", salary=" + salary + '}';
+	}
+}
