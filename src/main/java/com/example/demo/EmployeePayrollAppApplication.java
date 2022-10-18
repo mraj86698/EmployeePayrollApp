a819e267ece25fca3a00e64ebacb78948a8acbab
MohanRaj <mraj86698@gmail.com>
2022-10-18 19:43:28 +0530

    Added to Used for Lombok library for Logging

--- src/main/java/com/example/demo/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/demo/EmployeePayrollAppApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+/**
+ * Using Lombok For Logging
+ */
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		System.out.println("Welcome to EmployeePayroll APP");
+		log.info("Employee Payroll App has Started");
 	}
 }
