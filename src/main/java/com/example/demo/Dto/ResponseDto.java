00d48637d23de6c40d530de1fabecca163b1f0d5
MohanRaj <mraj86698@gmail.com>
2022-10-19 07:38:52 +0530

    Added to Determining the Logging Levels and Logging to console or file,Logging patterns

--- src/main/java/com/example/demo/Dto/ResponseDto.java
+++ src/main/java/com/example/demo/Dto/ResponseDto.java
@@ -1,5 +1,15 @@
 package com.example.demo.Dto;
 
+import lombok.Data;
+
+/**
+ * Using @Data is equivalent to
+ * 		using @code @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
+ * Generates getters for all fields, a useful toString method, and hashCode and
+ * 		equals implementations that check all non-transient fields.
+ * @author mraj
+ */
+@Data
 public class ResponseDto {
 
 	   private String message;
--- src/main/java/com/example/demo/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/demo/EmployeePayrollAppApplication.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
+
 /**
  * Using Lombok For Logging
  */
@@ -16,6 +17,8 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		System.out.println("Welcome to EmployeePayroll APP");
-		log.info("Employee Payroll App has Started");
+		log.info("Application Started");
+
+		log.info("Employee Payroll App has Started in {} Environment",context.getEnvrionment().getProperty("environment"));
 	}
 }
