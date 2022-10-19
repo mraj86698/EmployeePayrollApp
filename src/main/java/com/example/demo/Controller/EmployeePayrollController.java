e6dd8dc5f03f599790b1dc6e4a870781d8f961d8
MohanRaj <mraj86698@gmail.com>
2022-10-19 10:13:45 +0530

    Added Validation to Name Field And the Rest call can be validated

--- src/main/java/com/example/demo/Controller/EmployeePayrollController.java
+++ src/main/java/com/example/demo/Controller/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.example.demo.Controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -57,7 +59,7 @@ public class EmployeePayrollController {
      * @return
      */
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> addEmployeePayrollData(@RequestBody EmployeePayrollDto empPayrollDto)
+    public ResponseEntity<ResponseDto> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDto empPayrollDto)
     {
         EmployeePayrollData employeePayrollData = null;
         employeePayrollData = employeePayrollService.createEmployeePayrollData(empPayrollDto);
--- src/main/java/com/example/demo/Dto/EmployeePayrollDto.java
+++ src/main/java/com/example/demo/Dto/EmployeePayrollDto.java
@@ -1,16 +1,24 @@
 package com.example.demo.Dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
+
 /**
  * Using @Data is equivalent to
- * 		using @code @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
+ * using @code @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
  * Generates getters for all fields, a useful toString method, and hashCode and
- * 		equals implementations that check all non-transient fields.
- * @author mraj
+ * equals implementations that check all non-transient fields.
  */
 @Data
 public class EmployeePayrollDto {
+
+	@NotEmpty(message = "Employee name cannot be empty")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
 	public String empName;
+	@Min(value = 500, message = "Salary should be more than 500")
 	public long salary;
 
 	public EmployeePayrollDto(String empName, long salary) {
