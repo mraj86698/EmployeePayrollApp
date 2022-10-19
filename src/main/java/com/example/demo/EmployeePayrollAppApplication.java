9b5efac2307ba0508df111c770d079ae926d8a87
MohanRaj <mraj86698@gmail.com>
2022-10-19 09:15:12 +0530

    Added to Setting as Database Environment Varable Ant Testing

--- src/main/java/com/example/demo/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/demo/EmployeePayrollAppApplication.java
@@ -4,6 +4,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,10 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		System.out.println("Welcome to EmployeePayroll APP");
 		log.info("Application Started");
 
-		log.info("Employee Payroll App has Started in {} Environment",context.getEnvrionment().getProperty("environment"));
+		log.info("Employee Payroll App has Started in {} Environment",context.getEnvironment().getProperty("environment"));
+        log.info("Database : User Name is {}",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 }
